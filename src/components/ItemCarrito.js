4bc957a9a077385519f062c9f5a588fa6100c402
IgnaDev <arcortoon@gmail.com>
2022-06-06 20:37:09 -0300

    Todos los componentes y containers designados

---
+++ src/components/ItemCarrito.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ItemCarrito() {
+  return (
+    <div>
+        <h1>ItemCarrito</h1>
+    </div>
+  )
+}
---
+++ src/containers/Carrito.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Carrito() {
+  return (
+    <div>
+        <h1>Carrito</h1>
+    </div>
+  )
+}
